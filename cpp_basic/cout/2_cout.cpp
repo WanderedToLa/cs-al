0cbadef601a436a07fd4664243473f2bb24a486e
WanderedToLa <rladntjr9808@gmail.com>
2022-12-23 10:58:27 +0900

    Update: cout

---
+++ cpp_basic/cout/2_cout.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+string a , b;
+using namespace std;
+
+int main(){
+    cin >> a >> b;
+    cout << a << "\n";
+    cout << b << "\n";
+    return 0;
+}
+
+/**
+* Input - 김우석 입니다.
+* Output - 김우석
+           입니다.
+*/
