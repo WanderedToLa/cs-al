6fe5741039e6545300c0afbc7f2f9787497c464d
WanderedToLa <rladntjr9808@gmail.com>
2022-12-23 14:41:02 +0900

    Update: printf

---
+++ cpp_basic/printf/1_printf.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a = 1 , b = 2;
+int main(){
+    printf("홍철 %d : 지수 %d\n" , a , b);
+    return 0;
+}
+
+// 홍철 1 : 지수 2
+// 형식을 지정해서 출력 하려면 printf
---
+++ cpp_basic/printf/2_printf_precision.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+double a = 1.23456789;
+int b = 2;
+
+int main(){
+    printf("%.6lf\n" , a);
+    printf("%02d\n" , b);
+
+    return 0;
+}
