c157122dbd551dc89a0c5ef220ff6121f73b0c73
WanderedToLa <rladntjr9808@gmail.com>
2023-01-05 10:24:10 +0900

    Update: type

---
+++ cpp_basic/type/8_reverse.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string a = "It's hard to have a sore leg";
+    reverse(a.begin() , a.end());
+    cout << a << '\n';
+    reverse(a.begin() , a.end());
+    cout << a << '\n';
+    reverse(a.begin() + 3 , a.end());
+    cout << a << '\n';
+
+    return 0;
+}
+
+/*
+gel eros a evah ot drah s'tI
+It's hard to have a sore leg
+It'gel eros a evah ot drah s
+*/
