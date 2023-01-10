37532fe9256840b0cb647dedda652cf98327556b
WanderedToLa <rladntjr9808@gmail.com>
2023-01-10 10:42:57 +0900

    Update bool

---
+++ cpp_basic/method/4_bool.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 0이 아닌 값들은 true
+// 1 또는 0
+
+int main(){ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int a = -1;
+    cout << bool(a) << '\n';
+    a = 0;
+    cout << bool(a) << '\n';
+    a = 5;
+    cout << bool(a) << '\n';
+
+    return 0;
+}
