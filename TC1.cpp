1f8ff7c1aec9a4cb2e4d8a7b45e3074061883ae5
WanderedToLa <rladntjr9808@gmail.com>
2023-01-29 13:42:56 +0900

    Add cnt

--- TC1.cpp
+++ TC1.cpp
@@ -1,15 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n;
+int n , cnt;
 int main(){
     cin >> n;
     int a = 0;
     for (int i = 0; i < n; i++){
         for (int j = 0; j < i; j++){
-            a += i + j;
+            // a += i + j;
+            cnt++;
         }
     }
     cout << a << '\n';
+    cout << "cnt:" << cnt << '\n';
     return 0;
 }
