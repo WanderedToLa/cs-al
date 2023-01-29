543d5fc921fd5e441f895a0993a0f18abb6b12b2
WanderedToLa <rladntjr9808@gmail.com>
2023-01-29 12:48:30 +0900

    Remove: test.cpp

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h> // --- (1)
-using namespace std;// --- (2)
-string a;// --- (3)
-
-int main(){
-    cin >> a;// --- (4)
-    cout << a << "\n";// --- (5)
-    return 0; // - (6)
-}
