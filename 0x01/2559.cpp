c1fa5ab67f0cc93e707945b66a37ae27b2be80b6
Wooseok <rladntjr9808@gmail.com>
2024-07-27 16:28:54 +0900

    update 2559

--- 0x01/2559.cpp
+++ 0x01/2559.cpp
@@ -2,6 +2,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int temp[100004], presum[100004], N, K;
+
+// 입력된 N만큼의 누적합을 구한 후 
+// 입력된 K구간에 대한 합이므로 K만큼 뺀 부분부터 K까지의 구간의 합을 구하고 최댓값 비교
 int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> K;
+
+    for (int i = 1; i <= N; i++) {
+        cin >> temp[i];
+        presum[i] = presum[i - 1] + temp[i];
+    }
+
+    int maxtemp = -10000004;
+
+    for (int i = K; i <= N; i++) {
+        maxtemp = max(maxtemp , presum[i] - presum[i - K]);
+    }
+
+    cout << maxtemp << "\n";
+
     return 0;
 }
--- 0x01/prefixsum.cpp
+++ 0x01/prefixsum.cpp
@@ -1,14 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// 구간"에 대한 많은 "쿼리"가 나올 때 생각해야 될 것은 트리 또는 누적합
+// "구간"에 대한 많은 "쿼리"가 나올 때 생각해야 될 것은 트리 또는 누적합
+// a = [0, 3, 2, 4, 1] 예시로 a[1] = 3, a[2] = 2, a[3] = 4, a[4] = 1 이라 가정
+// psum[1] = psum[0] + a[1] = 0 + 3 = 3
+// psum[2] = psum[1] + a[2] = 3 + 2 = 5
+// psum[3] = psum[2] + a[3] = 5 + 4 = 9
+// psum[4] = psum[3] + a[4] = 9 + 1 = 10
 int a[100004], n, m, psum[100004], b, c;
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m;
 
-    for (int i = 1; i < n; i++){
+    for (int i = 1; i <= n; i++){
         cin >> a[i];
         psum[i] = psum[i - 1] + a[i];
     }
